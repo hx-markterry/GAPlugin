ad0315d1a9ad0e7bbef6579c441c561ca718c891
Mark Terry <mark.terry@holidayextras.com>
2014-09-10 14:28:34 +0100

    upgraded ga for android to v3

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -1,146 +1,167 @@
 package com.adobe.plugins;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
+//import com.google.analytics.tracking.android.*;
+import android.util.Log;
+import com.google.android.gms.common.*;
+import com.google.android.gms.analytics.*;
+import com.phonegap.helloworld.R;
+import org.apache.cordova.CordovaPlugin;          // modificato da org.apache.cordova.CordovaPlugin a org.apache.cordova.api.CordovaPlugin
+import org.apache.cordova.CallbackContext;          // modificato org.apache.cordova.CallbackContext a org.apache.cordova.api.CallbackContext
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.analytics.tracking.android.GAServiceManager;
-import com.google.analytics.tracking.android.GoogleAnalytics;
-import com.google.analytics.tracking.android.Tracker;
-import com.google.analytics.tracking.android.Transaction;
-import com.google.analytics.tracking.android.Transaction.Builder;
-import com.google.analytics.tracking.android.Transaction.Item;
 
 public class GAPlugin extends CordovaPlugin {
-	
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callback) {
-		GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
-		Tracker tracker = ga.getDefaultTracker(); 
-		if (action.equals("initGA")) {
-			try {
-				tracker = ga.getTracker(args.getString(0));
-				GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));
-				ga.setDefaultTracker(tracker);
-				callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		} else if (action.equals("exitGA")) {
-			try {
-				GAServiceManager.getInstance().dispatch();
-				callback.success("exitGA");
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		} else if (action.equals("trackEvent")) {
-			try {
-				tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));
-				callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		} else if (action.equals("trackPage")) {
-			try {
-				tracker.sendView(args.getString(0));
-				callback.success("trackPage - url = " + args.getString(0));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		} else if (action.equals("setVariable")) {
-			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
-				callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		else if (action.equals("setDimension")) {
-			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
-				callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		else if (action.equals("setMetric")) {
-			try {
-				tracker.setCustomMetric(args.getInt(0), args.getLong(1));
-				callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		else if(action.equals("trackTransaction")){
-			try {
-				this.__trackTransaction(args.getJSONObject(0));
-				callback.success("trackTransaction = " + args.getString(0));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		else if(action.equals("trackCaughtException")){
-			try {
-				this.__trackException(args.getString(0), false);
-				callback.success("trackCaughtException = " + args.getString(0));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		else if(action.equals("trackUncaughtException")){
-			try {
-				this.__trackException(args.getString(0), true);
-				callback.success("trackUncaughtException = " + args.getString(0));
-				return true;
-			} catch (final Exception e) {
-				callback.error(e.getMessage());
-			}
-		}
-		return false;
-	}
-	
-	private void __trackTransaction(JSONObject jObj) throws JSONException{
-		Builder transBuilder = new Transaction.Builder(
-			jObj.getString("transactionId"),	// (String) Transaction Id, should be unique.
-			jObj.getLong("orderTotal")	// (long) Order total (in micros)
-		);
-		transBuilder.setAffiliation(jObj.getString("affiliation"));	// (String) Affiliation
-		transBuilder.setTotalTaxInMicros(jObj.getLong("totalTax"));	// (long) Total tax (in micros)
-		transBuilder.setShippingCostInMicros(jObj.getLong("shippingCost"));	// (long) Total shipping cost (in micros)
-		Transaction trans = transBuilder.build();
-		JSONArray items = jObj.getJSONArray("items");
-		for(int i = 0 ; i < items.length(); i++){	//add all the items to the transaction
-			JSONObject item = items.getJSONObject(i);
-			Item.Builder itemBuilder = new Item.Builder(
-				item.getString("sku"),	// (String) Product SKU
-				item.getString("name"),	// (String) Product name
-				item.getLong("price"),	// (long) Product price (in micros)
-				item.getLong("quantity")	// (long) Product quantity
-			);
-			itemBuilder.setProductCategory(item.getString("category"));	// (String) Product category
-			trans.addItem(itemBuilder.build());
-		}
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException{
+        GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
+        Tracker tracker=ga.newTracker("UA-44301016-1");
 
-		GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
-		Tracker tracker = ga.getDefaultTracker(); 
-		tracker.sendTransaction(trans); // Send the transaction
-	}
-	
-	private void __trackException(String message, Boolean fatal){
-		GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
-		Tracker tracker = ga.getDefaultTracker(); 
-		tracker.sendException(message, fatal); // Send the exception
-	}
+        if (action.equals("initGA")) {
+            try {
+                tracker = ga.newTracker(args.getString(0));
+                ga.setLocalDispatchPeriod(args.getInt(1));
+                callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        } else if (action.equals("exitGA")) {
+            try {
+                /*GAServiceManager.getInstance().dispatch(); */
+                ga.dispatchLocalHits();
+
+                callback.success("exitGA");
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        } else if (action.equals("trackEvent")) {
+            try {
+                /*tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));  */
+                tracker.send(new HitBuilders.EventBuilder()
+                                .setCategory(args.getString(0))
+                                .setAction(args.getString(1))
+                                .setLabel(args.getString(2))
+                                .setValue(args.getLong(3))
+                                //.setValue(args.getString(3))
+                                .build());
+                        //MapBuilder.createEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3)).build());
+                callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        } else if (action.equals("trackPage")) {
+            try {
+                /*tracker.sendView(args.getString(0));*/
+                //tracker.setPage(args.getString(0));
+                tracker.setScreenName(args.getString(0));
+                tracker.send(new HitBuilders.AppViewBuilder().build());
+
+                        //MapBuilder.createAppView().set(Fields.SCREEN_NAME,args.getString(0)).build());
+                callback.success("trackPage - url = " + args.getString(0));
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        } /*else if (action.equals("setVariable")) {
+            try {
+                *//*tracker.setCustomDimension(args.getInt(0), args.getString(1));*//*
+                // tracker.set(Fields.customDimension(args.getInt(0)), args.getString(1));
+                tracker.send(new HitBuilders.AppViewBuilder()
+                                .setCustomMetric(args.getInt(0), args.getLong(1))
+                                .build()
+                );
+
+                callback.success("setVariable passed - index = " + args.getString(0) + "; value = " + args.getString(1));
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        }*/
+        else if (action.equals("setDimension")) {
+            try {
+                /*tracker.setCustomDimension(args.getInt(0), args.getString(1));*/
+                tracker.send(new HitBuilders.AppViewBuilder()
+                                .setCustomDimension(args.getInt(0), args.getString(1))
+                                .build()
+                );
+                callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        }
+        else if (action.equals("setMetric")) {
+            try {
+                /*tracker.setCustomMetric(args.getInt(0), args.getLong(1));*/
+                tracker.send(new HitBuilders.AppViewBuilder()
+                                .setCustomMetric(args.getInt(0), args.getLong(1))
+                                .build()
+                );
+                callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
+                return true;
+            } catch (final Exception e) {
+                callback.error(e.getMessage());
+            }
+        }
+        else if (action.equals("trackSocial")){
+            try{
+                tracker.send(new HitBuilders.SocialBuilder().setNetwork(args.getString(0))
+                                                            .setAction(args.getString(1))
+                                                            .setTarget(args.getString(2))
+                                                            .build());
+                callback.success("trackSocial - Network = " + args.getString(0) + "; action = " + args.getString(1) + "; target = " + args.getString(2));
+            } catch(final Exception e){
+                callback.error(e.getMessage());
+            }
+        }
+        else if (action.equals("trackEcommerceTransaction")){
+            try{
+                tracker.send(new HitBuilders.TransactionBuilder().setTransactionId(args.getString(0))
+                        .setAffiliation(args.getString(1))
+                        .setRevenue(args.getDouble(2))
+                        .setTax(args.getDouble(3))
+                        .setShipping(args.getDouble(4))
+                        .setCurrencyCode(args.getString(5))
+                        .build());
+
+                callback.success("trackSocial - Transaction ID = "+ args.getString(0) +
+                                 " Affiliation "+args.getString(1) +
+                                 " Revenue " + args.getDouble(2)+
+                                 " Tax " +   args.getDouble(3)+
+                                 " Shipping " + args.getDouble(4)+
+                                 " Currency code " + args.getString(5)
+                                );
+
+            } catch(final Exception e){
+                callback.error(e.getMessage());
+            }
+        }
+        else if (action.equals("trackEcommerceItem")){
+            try{
+                tracker.send(new HitBuilders.ItemBuilder().setTransactionId(args.getString(0))          // (String) Transaction ID
+                        .setName(args.getString(1))          // (String) Product name
+                        .setSku(args.getString(2))          // (String) Product SKU
+                        .setCategory(args.getString(3))          // (String) Product category
+                        .setPrice(args.getDouble(4))          // (Double) Product price
+                        .setQuantity(args.getLong(5))            // (Long) Product quantity
+                        .setCurrencyCode(args.getString(6))           // (String) Currency code
+                        .build());
+
+                callback.success("trackSocial - Transaction ID = "+ args.getString(0) +
+                                 " Name "+args.getString(1) +
+                                 " SKU " + args.getString(2)+
+                                 " Category " +   args.getString(3)+
+                                 " Price " + args.getDouble(4)+
+                                 " Quantity " + args.getString(5) +
+                                 " Currency code "+args.getString(6)
+                                );
+            } catch(final Exception e){
+                callback.error(e.getMessage());
+            }
+        }
+
+        return false;
+    }
 }
-
